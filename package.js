1e2eb6745d2a960e48b789604e48b09b8959c4d8
capp997 <63300856+capp997@users.noreply.github.com>
2025-08-26 18:01:36 -0500

    Update package.js

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 {
-  "name": "calendar-backend",
+  "name": "cc-calendar-backend",
   "version": "1.0.0",
   "main": "server.js",
-  "type": "module",
   "scripts": {
     "start": "node server.js"
   },
   "dependencies": {
     "express": "^4.18.2",
-    "googleapis": "^131.0.0",
+    "cors": "^2.8.5",
+    "googleapis": "^115.0.0",
     "body-parser": "^1.20.2"
-  },
+  }
   "engines": {
     "node": "18.x"
   }
