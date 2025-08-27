c2abda5b5b0e136746a0b95a857aceba1939acad
capp997 <63300856+capp997@users.noreply.github.com>
2025-08-26 21:42:03 -0500

    Update package.js

--- package.js
+++ package.js
@@ -1,17 +1,19 @@
 {
-  "name": "cc-calendar-backend",
+  "name": "citas-cc-backend",
   "version": "1.0.0",
+  "description": "Backend para gestionar citas C&C Family Landscaping",
   "main": "server.js",
   "scripts": {
     "start": "node server.js"
   },
+  "engines": {
+    "node": "20.x"
+  },
   "dependencies": {
     "express": "^4.18.2",
+    "body-parser": "^1.20.2",
     "cors": "^2.8.5",
-    "googleapis": "^115.0.0",
-    "body-parser": "^1.20.2"
-  }
-  "engines": {
-    "node": "18.x"
+    "googleapis": "^118.0.0",
+    "dotenv": "^16.3.1"
   }
 }
